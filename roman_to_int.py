14ac5d83bc4ebeccf1a412480e705d8af264cc5f
Emon Rahman <246264230+emon-rhman@users.noreply.github.com>
2025-12-07 23:58:56 +0600

    Add files via upload

---
+++ roman_to_int.py
@@ -0,0 +1,24 @@
+def romanToInt(s: str) -> int:
+    roman = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
+    }
+
+    total = 0
+    prev = 0
+
+    for char in reversed(s):
+        value = roman[char]
+
+        if value < prev:
+            total -= value
+        else:
+            total += value
+
+        prev = value
+
+    return total
+
+
+s = input("Enter a Roman numeral: ").upper()
+print(romanToInt(s))
